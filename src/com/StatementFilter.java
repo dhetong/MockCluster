f61d92666b9a693db05b6dd9dd5f48f7dd232da7
Hetong Dai <hetongdai@hetongs-mbp.wireless.concordia.ca>
2021-08-24 17:22:27 -0400

    2021.8.24

--- src/com/StatementFilter.java
+++ src/com/StatementFilter.java
@@ -1,10 +1,21 @@
 package com;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.eclipse.jdt.core.dom.Statement;
 
 public class StatementFilter {
 	public void stmFilter(Statement s) {
 		if(s.getNodeType() == Statement.EXPRESSION_STATEMENT) {
+			Pattern PatternWhen = Pattern.compile("when\\(([^}]*)\\)\\.");
+			Matcher matcher = PatternWhen.matcher(s.toString());
+			if(matcher.find()) {
+				if(s.toString().contains("doReturn")) {
+				}
+				else if(s.toString().contains("thenReturn")) {
+				}
+			}
 		}
 	}
 }
